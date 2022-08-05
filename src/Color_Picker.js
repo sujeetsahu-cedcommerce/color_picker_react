ac74bd22d4444a61ea7cf4691c61099484a3125a
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-08-05 10:05:13 +0530

    color_picker_task

--- src/Color_Picker.js
+++ src/Color_Picker.js
@@ -1,29 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Color_Picker() {
-    const [color_value,set_color_value] = useState('');
-
-    const get_Color = (event) => {
-        console.log(event.target.value);
-        set_color_value(event.target.value);
-        console.log(color_value);
+  const [color_value, set_color_value] = useState("#");
+  const [R,setR] = useState('');
+  const [G,setG] = useState('');
+  const [B,setB] = useState('');
+  const get_R = (event) => {
+    if(/^[0-9a-f]+$/.test(event.target.value)){
+      setR(event.target.value);
     }
+  }
 
-    const get_box_value = (event) => {
-        
-        if(event.target.value.length === 7){
-            set_color_value(event.target.value);
-        }
+  const get_G = (event) => {
+    if(/^[0-9a-f]+$/.test(event.target.value)){
+      setG(event.target.value);
     }
+  }
+
+  const get_B = (event) => {
+    if(/^[0-9a-f]+$/.test(event.target.value)){
+      setB(event.target.value);
+    }
+  }
+
+  const get_RGB_Color = (event) => {
+    set_color_value(`#${R}${G}${B}`)
+  };
+  console.log(color_value);
+
+  const get_box_value = (event) => {
+     set_color_value(event.target.value);
+  };
 
   return (
-    <div style={{height:"100vh",width:"100vw",backgroundColor:color_value}}>
-         <h1>set Color </h1>
-         enter hexa decimal color code : <input type="text" onKeyUp={get_box_value} /><br></br>
-         <input type="color" onChange={get_Color} value={color_value}/>
-    </div>
+    <div
+      style={{ height: "100vh", width: "100vw", backgroundColor: color_value }}>
+      <input type="color" onChange={get_box_value} value={color_value} />
 
-  )
+      <input type="text" placeholder="R" onKeyUp={get_R} maxLength={2}/>
+      <input type="text" placeholder="g" onKeyUp={get_G} maxLength={2}/>
+      <input type="text" placeholder="b" onKeyUp={get_B} maxLength={2}/>
+      <button onClick={get_RGB_Color}>Change Color</button>
+    </div>
+  );
 }
 
-export default Color_Picker
+export default Color_Picker;
